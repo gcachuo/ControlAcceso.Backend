bca443b134a6101dc4d5bd8433cc6f4af49dd499
Memo Cachú <gcachu.o@gmail.com>
2024-09-24 00:46:19 -0600

    [CAF-6] Add test

--- ControlAcceso.Tests/Endpoints/RolesTests.cs
+++ ControlAcceso.Tests/Endpoints/RolesTests.cs
@@ -1,21 +1,31 @@
-﻿using ControlAcceso.Endpoints.Roles;
+﻿using ControlAcceso.Data.Model;
+using ControlAcceso.Data.Roles;
+using ControlAcceso.Endpoints.Roles;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Endpoint = ControlAcceso.Endpoints.Roles.Endpoint;
 
 namespace ControlAcceso.Tests.Endpoints
 {
     public class RolesTests
     {
+        private readonly Mock<IRolesDbContext> _dbContextMock = new(MockBehavior.Strict);
+
         [Fact]
-        public void Should_Create_Role()
+        public void Should_Create_Role_Successfully()
         {
             //Arrange
+            var request = new Request { Name = "Test" };
+
             //Mock
+            _dbContextMock
+                .Setup(x => x.InsertRole(It.IsAny<RoleModel>()));
+
             //Act
-            var endpoint = new Endpoint();
-            var result = endpoint.CreateRole() as ObjectResult;;
+            var endpoint = new Endpoint(_dbContextMock.Object);
+            var result = endpoint.CreateRole(request) as ObjectResult;
 
             //Assert
             result?.StatusCode.Should().Be(StatusCodes.Status200OK, result.Value?.ToString());
