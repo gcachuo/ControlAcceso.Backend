f9e341fefe8a879f2a32cfd7308e45418d60c0f2
IvanJGG <ivangallardoguevara@gmail.com>
2024-09-26 18:14:40 -0600

    Add test to increase coverage

--- ControlAcceso.Tests/Data/UsersDbContextTests.cs
+++ ControlAcceso.Tests/Data/UsersDbContextTests.cs
@@ -21,5 +21,22 @@ namespace ControlAcceso.Tests.Data
             //Assert
             _dbServiceMock.Verify(x=>x.ExecuteNonQuery(It.IsAny<string>(),It.IsAny<Dictionary<string,dynamic>>()));
         }
+
+        [Fact] 
+        public void Should_Edit_User_Successfully()
+        {
+            //Arrange
+            var idUser = 1;
+            var user = new UserModel();
+
+            //Mock
+
+            //Act
+            var context = new UsersDbContext(_dbServiceMock.Object);
+            context.UpdateUser(user, idUser);
+
+            //Assert
+           _dbServiceMock.Verify(x=>x.ExecuteNonQuery(It.IsAny<string>(),It.IsAny<Dictionary<string,dynamic>>()));
+        }
     }
 }
