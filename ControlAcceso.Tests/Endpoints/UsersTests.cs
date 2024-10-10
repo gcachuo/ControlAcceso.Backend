4c50a9c07bd63191ec7eee5cd96ab5de4a350f7b
Memo Cachú <gcachu.o@gmail.com>
2024-10-10 14:39:26 -0600

    Fix build

--- ControlAcceso.Tests/Endpoints/UsersTests.cs
+++ ControlAcceso.Tests/Endpoints/UsersTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using ControlAcceso.Data.Model;
+using ControlAcceso.Data.RefreshTokens;
 using ControlAcceso.Data.Users;
 using ControlAcceso.Endpoints.Users;
 using Microsoft.AspNetCore.Http;
@@ -13,44 +14,47 @@ namespace ControlAcceso.Tests.Endpoints
 {
     public class UsersTests
     {
-        private readonly Mock<IUsersDbContext> _usersDbContext=new(MockBehavior.Strict);
-        
+        private readonly Mock<IUsersDbContext> _usersDbContext = new(MockBehavior.Strict);
+        private readonly Mock<IRefreshTokensDbContext> _refreshTokensDbContext = new(MockBehavior.Strict);
+
         [Fact]
         public void Should_Register_User_Successfully()
         {
             //Arrange
-            var request = new Request(){Password = "123456"};
-            
+            var request = new UserRequest() { Password = "123456" };
+
             //Mock
             _usersDbContext.Setup(x => x.InsertUser(It.IsAny<UserModel>()));
-            
+
             //Act
-            var endpoint = new Endpoint(_usersDbContext.Object);
-            var result = endpoint.RegisterUser(request) as ObjectResult;;
+            var endpoint = new Endpoint(_usersDbContext.Object, _refreshTokensDbContext.Object);
+            var result = endpoint.RegisterUser(request) as ObjectResult;
+            ;
 
             //Assert
             result?.StatusCode.Should().Be(StatusCodes.Status200OK, result.Value?.ToString());
-            (result!.Value as Response)!.Message.Should().Be("OK");
+            (result!.Value as UserResponse)!.Message.Should().Be("OK");
         }
-        
+
         [Fact]
         public void When_User_Is_Already_Registered_Then_Exception_Is_Thrown()
         {
             //Arrange
-            var request = new Request(){Password = "123456"};
-            
+            var request = new UserRequest() { Password = "123456" };
+
             //Mock
             _usersDbContext.Setup(x => x.InsertUser(It.IsAny<UserModel>())).Throws<DataException>(() => new("Usuario duplicado."));
-            
+
             //Act
-            var endpoint = new Endpoint(_usersDbContext.Object);
-            var result = endpoint.RegisterUser(request) as ObjectResult;;
+            var endpoint = new Endpoint(_usersDbContext.Object, _refreshTokensDbContext.Object);
+            var result = endpoint.RegisterUser(request) as ObjectResult;
+            ;
 
             //Assert
             result?.StatusCode.Should().Be(StatusCodes.Status400BadRequest, result.Value?.ToString());
-            (result!.Value as Response)!.Message.Should().Be("Usuario duplicado.");
+            (result!.Value as UserResponse)!.Message.Should().Be("Usuario duplicado.");
         }
-        
+
         [Fact]
         public void Should_Edit_User_Successfully()
         {
@@ -58,27 +62,26 @@ namespace ControlAcceso.Tests.Endpoints
             const int idUser = 1;
 
             //Mock
-            var request = new Request(){Password = "123456"};
+            var request = new UserRequest() { Password = "123456" };
             // Mock setup for UpdateUser
 
             _usersDbContext.Setup(x => x.UpdateUser(It.IsAny<UserModel>(), It.IsAny<int>()))
-               .Verifiable();  // Verifica que el método fue llamado
+                .Verifiable(); // Verifica que el método fue llamado
             // Suponiendo que el método UpdateUser retorna un booleano
 
-            
+
             //Act
-            var endpoint = new Endpoint(_usersDbContext.Object);
+            var endpoint = new Endpoint(_usersDbContext.Object, _refreshTokensDbContext.Object);
             var result = endpoint.EditUser(idUser, request) as ObjectResult;
 
             //Assert
             result?.StatusCode.Should().Be(StatusCodes.Status200OK, result.Value?.ToString());
-            (result!.Value as Response)!.Message.Should().Be("Usuario actualizado correctamente");
+            (result!.Value as UserResponse)!.Message.Should().Be("Usuario actualizado correctamente");
 
             //Verifica que el mock haya sido llamado
             _usersDbContext.Verify(x => x.UpdateUser(It.IsAny<UserModel>(), It.IsAny<int>()), Times.Once);
-
         }
-        
+
         [Fact]
         public void Should_Get_User_Successfully()
         {
@@ -86,15 +89,15 @@ namespace ControlAcceso.Tests.Endpoints
             const int idUser = 1;
 
             //Mock
-            _usersDbContext.Setup(x=>x.SelectUser(idUser)).Returns(new UserModel());
-            
+            _usersDbContext.Setup(x => x.SelectUser(idUser)).Returns(new UserModel());
+
             //Act
-            var endpoint = new Endpoint(_usersDbContext.Object);
+            var endpoint = new Endpoint(_usersDbContext.Object, _refreshTokensDbContext.Object);
             var result = endpoint.GetUser(idUser) as ObjectResult;
 
             //Assert
             result?.StatusCode.Should().Be(StatusCodes.Status200OK, result.Value?.ToString());
-            (result!.Value as Response)!.Message.Should().Be("OK");
+            (result!.Value as UserResponse)!.Message.Should().Be("OK");
         }
     }
 }
