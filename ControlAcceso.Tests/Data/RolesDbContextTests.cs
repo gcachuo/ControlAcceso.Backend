ed2cc87c82293663711890fdb89e9fcd4d85a53c
IvanJGG <ivangallardoguevara@gmail.com>
2024-10-14 19:56:06 -0600

    CAF-35 Add DbContext Testing

--- ControlAcceso.Tests/Data/RolesDbContextTests.cs
+++ ControlAcceso.Tests/Data/RolesDbContextTests.cs
@@ -22,5 +22,56 @@ namespace ControlAcceso.Tests.Data
             //Assert
             _dbServiceMock.Verify(x=>x.ExecuteNonQuery(It.IsAny<string>(),It.IsAny<Dictionary<string,dynamic>>()));
         }
+            
+
+    
+        [Fact]
+        public void SelectRole_Returns_List_Of_Roles()
+        {
+            // Arrange
+            var mockDbService = new Mock<IDbService>();
+
+
+            var fakeRows = new List<Dictionary<string, dynamic>>()
+            {
+                new Dictionary<string, dynamic> { { "name", "Admin" }, { "id", "1" } },
+                new Dictionary<string, dynamic> { { "name", "User" }, { "id", "2" } }
+            };
+
+           
+            mockDbService.Setup(db => db.ExecuteReader("SELECT * FROM Roles", It.IsAny<Dictionary<string, dynamic>>()))
+                        .Returns(fakeRows);
+
+            var dbContext = new RolesDbContext(mockDbService.Object);
+
+            // Act
+            var result = dbContext.SelectRole();
+
+            // Assert
+            Assert.NotNull(result); 
+            Assert.Equal(2, result.Count()); 
+            Assert.Equal("Admin", result.First().Name); 
+            Assert.Equal("1", result.First().Id); 
+        }
+
+        [Fact]
+        public void SelectRole_Returns_EmptyList()
+        {
+            // Arrange
+            var mockDbService = new Mock<IDbService>();
+
+            
+            mockDbService.Setup(db => db.ExecuteReader("SELECT * FROM Roles", It.IsAny<Dictionary<string, dynamic>>()))
+                        .Returns(new List<Dictionary<string, dynamic>>());
+
+            var dbContext = new RolesDbContext(mockDbService.Object);
+
+            // Act
+            var result = dbContext.SelectRole();
+
+            // Assert
+            Assert.NotNull(result); 
+            Assert.Empty(result);
+        }
     }
 }
